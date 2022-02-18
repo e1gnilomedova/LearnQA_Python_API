1f6ee29c32fb2cc4f4803854c8c94d1300a3a43c
e1gnilomedova <gnilomyodova.liza@yandex.ru>
2022-02-18 23:56:41 +0300

    Добавлена в файл base_case новая функция prepare_registration_data. Оптимизирован код lesson4_Ex16

--- tests/lesson4_Ex16.py
+++ tests/lesson4_Ex16.py
@@ -4,21 +4,10 @@ from lib.assertions import Assertions
 from datetime import datetime
 
 class TestUserRegister(BaseCase):
-    def setup(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
 
 #Создаем нового юзера, чтобы получить его id
     def test_get_user_details_of_another_user(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
@@ -36,7 +25,6 @@ class TestUserRegister(BaseCase):
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
-
 
 #Отправляем id первого юзера и куки, токен от второго
         response2 = requests.get(
