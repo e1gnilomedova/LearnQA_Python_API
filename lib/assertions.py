68f2a83fdcc2be44b2d16ab0708c4e4d54746ee2
e1gnilomedova <gnilomyodova.liza@yandex.ru>
2022-02-18 22:29:47 +0300

    lesson4_Ex16, добавлена в файл assertion новая проверка assert_json_has_not_keys

--- lib/assertions.py
+++ lib/assertions.py
@@ -1,6 +1,6 @@
 from requests import Response
-# from requests import Request
 import json
+
 class Assertions:
     @staticmethod
     def assert_json_value_by_name(response: Response, name, expected_value, error_message):
@@ -21,13 +21,41 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it is present"
+
+
+# Добавлено для ДЗ 16
+    @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it is present"
+
+
+    @staticmethod
     def assert_code_status(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
 
-   # Добавлено для ДЗ 15.1, 15.3, 15.4
+# Добавлено для ДЗ 15.1, 15.3, 15.4
     @staticmethod
     def assert_response_content(response: Response, expected_response_content,):
         assert response.content.decode("utf-8") == expected_response_content, \
             f"Unexpected content! Expected: {expected_response_content}. Actual: {response.content}"
-
---
+++ tests/lesson4_Ex16.py
@@ -0,0 +1,50 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+class TestUserRegister(BaseCase):
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+#Создаем нового юзера, чтобы получить его id
+    def test_get_user_details_of_another_user(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+        user_id_from_register_user = self.get_json_value(response, "id")
+
+
+#Авторизовываемся вторым пользователем, чтобы получить его куки и токен
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+
+#Отправляем id первого юзера и куки, токен от второго
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id_from_register_user}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_has_key(response2, "username")
+        expected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response2, expected_fields)
+        Assertions.assert_code_status(response2, 401)
