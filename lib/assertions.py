fe8fe52170c42102704aab739bbae5ff004a69ec
e1gnilomedova <gnilomyodova.liza@yandex.ru>
2022-02-19 21:38:05 +0300

    lesson4_Ex17, добавлена новая проверка 'assert_json_value_by_name_is_not_changed' в файл assertions.

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,6 +11,14 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
 
+# Добавлено для ДЗ 17
+    def assert_json_value_by_name_is_not_changed(response: Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+        assert response_as_dict[name] != expected_value, error_message
 
     @staticmethod
     def assert_json_has_key(response: Response, name):
@@ -38,7 +46,6 @@ class Assertions:
         assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it is present"
 
 
-# Добавлено для ДЗ 16
     @staticmethod
     def assert_json_has_not_keys(response: Response, names: list):
         try:
@@ -54,7 +61,7 @@ class Assertions:
         assert response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
 
-# Добавлено для ДЗ 15.1, 15.3, 15.4
+
     @staticmethod
     def assert_response_content(response: Response, expected_response_content,):
         assert response.content.decode("utf-8") == expected_response_content, \
