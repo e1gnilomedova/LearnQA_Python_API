65d52d9782fbdfd6743dadadd3ef0a70456db48a
e1gnilomedova <gnilomyodova.liza@yandex.ru>
2022-04-15 11:27:58 +0300

    Добавление тегов allure.link allure.severity

--- lesson3_Ex10.py
+++
@@ -1,6 +0,0 @@
-class TestExample:
-    def test_check_phrase(self):
-        phrase = input("Set a phrase: ")
-        a = len(phrase)
-        expected_sum = 15
-        assert a < expected_sum, f"Во фразе: '{phrase}' больше или равно {expected_sum} символов"
--- lib/base_case.py
+++ lib/base_case.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
-
 from requests import Response
+
 class BaseCase:
     def get_cookie (self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
